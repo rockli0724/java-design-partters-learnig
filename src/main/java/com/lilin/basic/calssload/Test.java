d6f281c2532ba65b1df8d6470b134a88d0eee195
rockli0724 <819645542@qq.com>
2019-08-04 16:22:29 +0800

    feat: 类加载顺序

---
+++ src/main/java/com/lilin/basic/calssload/Test.java
@@ -0,0 +1,12 @@
+package com.lilin.basic.calssload;
+
+/**
+ * @author lilin
+ * @Title: Test
+ * @date 2019/8/4下午4:10
+ */
+public class Test {
+    public static void main(String[] args) {
+        A2 a2 = new A2();
+    }
+}
